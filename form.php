4b6b7cf96dffe80b7a53beb9d815c62d23501bc3
keashynnaidoo <keashyn.naidoo@my.jcu.edu.au>
2021-05-18 11:45:23 +0800

    commit

---
+++ form.php
@@ -0,0 +1,14 @@
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+
+Email: <input type="text" name="email" value="<?php echo $email;?>">
+
+ProductInterests:<?php if(isset($_POST["submit"])){
+                 echo $_POST["product"]."<br>";
+                 }
+                 else{
+                                  ?
+
+Birthday:<?php require "../class.datepicker.php"; $db=new datepicker(); $db->firstDayOfWeek = 1;  $db->dateFormat = "m d Y"; ?>
+
+Message: <textarea name="comment" rows="4"><?php echo $comment;?></textarea>
+
